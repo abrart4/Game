47a87160fd175926e42fc0bd62de12a8f2fba3d7
AbrarTaseen <abrart4@nycstudents.net>
2025-10-03 11:50:03 -0400

    Progress

---
+++ src/Game.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Game {
+    private Level levelOne;
+    private Level levelTwo;
+    private Level levelThree;
+    private boolean bonus;
+
+    /** Postcondition: All instance variables have been initialized. */
+    public Game() {
+        levelOne = new Level();
+        levelTwo = new Level();
+        levelThree = new Level();
+    }
+
+    public Level getLevel(int i) {
+        if (i == 1) return levelOne;
+        if (i == 2) return levelTwo;
+        if (i == 3) return levelThree;
+        return null;
+    }
+
+    /** Returns true if this game is a bonus game and returns false otherwise */
+    public boolean isBonus() {
+        return bonus;
+    }
+    public void makeBonus() {
+        bonus = true;
+    }
+
+    /** Simulates the play of this Game (consisting of three levels) and updates all relevant
+     * game data
+     */
+    public void play() {
+        int points = -1;
+        while (points != ) {
+            System.out.println("Give me a number from 1 to 300. It will be important...");
+            points = new Scanner(System.in)
+        }
+    }
+
+    /** Returns the score earned in the most recently played game, as described in part (a) */
+    public int getScore() {
+        int points = 0;
+        if (levelOne.goalReached()) {
+            points += levelOne.getPoints();
+            if (levelTwo.goalReached()) {
+                points += levelTwo.getPoints();
+                if (levelThree.goalReached()) {
+                    points += levelThree.getPoints();
+                }
+            }
+        }
+        if (isBonus()) {
+            points *= 3;
+        }
+        return points;
+    }
+
+    /** Simulates the play of num games and returns the highest score earned, as
+     * described in part (b)
+     * Precondition: num > 0
+     */
+    public int playManyTimes(int num) {
+        return -1;
+    }
+}
